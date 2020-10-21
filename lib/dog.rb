aceabde4cfa210a58a6950e7a34bc18a94cd139b
Eric Lombardo <eric.m.lombardo@gmail.com>
2020-10-21 19:59:51 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+# defines a class called Dog
+# creates #writer for name
+# creates #reader for name
+# creats #bark that puts "woof!" when called on instance of Dog
+
+class Dog(name)
+  def name=(name)
+    @name = name
+  end
+end
